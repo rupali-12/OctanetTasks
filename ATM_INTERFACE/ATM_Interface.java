3be35d1f1501321738de966da1cdc1b89ba6eff0
rupali-12 <rupalisharma1009@gmail.com>
2023-10-30 18:33:07 +0530

    task updated

--- ATM_INTERFACE/ATM_Interface.java
+++ ATM_INTERFACE/ATM_Interface.java
@@ -1,4 +1,4 @@
-// *************************TASK_2  **********************************
+// *************************TASK_STATEMENT  **********************************
 // 1.Create a class to represent the ATM machine.
 // 2. Design the user interface for the ATM, including options such as withdrawing, depositing, and
 // checking the balance.
